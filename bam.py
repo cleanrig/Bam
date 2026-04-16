eea5f62c9d99215fd4eb7b017330b2e47ce04741
cleanrig <thebestestsheep@gmail.com>
2026-04-16 14:53:39 -0400

    Add files via upload

--- bam.py
+++ bam.py
@@ -1,55 +1,56 @@
 userin = input("What text do you want to print?")
 CP850list = []
-if userin == "a":
-    CP850list.append('97')
-if userin == "b":
-    CP850list.append('98')
-if userin == "c":
-    CP850list.append('99')
-if userin == "d":
-    CP850list.append('100')
-if userin == "e":
-    CP850list.append('101')
-if userin == "f":
-    CP850list.append('102')
-if userin == "g":
-    CP850list.append('103')
-if userin == "h":
-    CP850list.append('104')
-if userin == "i":
-    CP850list.append('105')
-if userin == "j":
-    CP850list.append('106')
-if userin == "k":
-    CP850list.append('107')
-if userin == "l":
-    CP850list.append('108')
-if userin == "m":
-    CP850list.append('109')
-if userin == "n":
-    CP850list.append('110')
-if userin == "o":
-    CP850list.append('111')
-if userin == "p":
-    CP850list.append('112')
-if userin == "q":
-    CP850list.append('113')
-if userin == "r":
-    CP850list.append('114')
-if userin == "s":
-    CP850list.append('115')
-if userin == "t":
-    CP850list.append('116')
-if userin == "u":
-    CP850list.append('117')
-if userin == "v":
-    CP850list.append('118')
-if userin == "w":
-    CP850list.append('119')
-if userin == "x":
-    CP850list.append('120')
-if userin == "y":
-    CP850list.append('121')
-if userin == "z":
-    CP850list.append('122')
+for letter in userin:
+    if letter == "a":
+        CP850list.append('97')
+    if letter == "b":
+        CP850list.append('98')
+    if letter == "c":
+        CP850list.append('99')
+    if letter == "d":
+        CP850list.append('100')
+    if letter == "e":
+        CP850list.append('101')
+    if letter == "f":
+        CP850list.append('102')
+    if letter == "g":
+        CP850list.append('103')
+    if letter == "h":
+        CP850list.append('104')
+    if letter == "i":
+        CP850list.append('105')
+    if letter == "j":
+        CP850list.append('106')
+    if letter == "k":
+        CP850list.append('107')
+    if letter == "l":
+        CP850list.append('108')
+    if letter == "m":
+        CP850list.append('109')
+    if letter == "n":
+        CP850list.append('110')
+    if letter == "o":
+        CP850list.append('111')
+    if letter == "p":
+        CP850list.append('112')
+    if letter == "q":
+        CP850list.append('113')
+    if letter == "r":
+        CP850list.append('114')
+    if letter == "s":
+        CP850list.append('115')
+    if letter == "t":
+        CP850list.append('116')
+    if letter == "u":
+        CP850list.append('117')
+    if letter == "v":
+        CP850list.append('118')
+    if letter == "w":
+        CP850list.append('119')
+    if letter == "x":
+        CP850list.append('120')
+    if letter == "y":
+        CP850list.append('121')
+    if letter == "z":
+        CP850list.append('122')
 print(CP850list)
